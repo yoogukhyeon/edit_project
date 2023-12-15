05d6dcf94e15f711141d8e0406c27cb666c8695e
유국현 <yoogukhyeon@yugughyeon-ui-iMac.local>
2023-12-15 18:03:01 +0900

    build

--- src/seo/SEOMetaTag.tsx
+++ src/seo/SEOMetaTag.tsx
@@ -10,12 +10,17 @@ const SEOMetaTag = (props: any) => {
       {/* <meta name="description" content={props.description} />
       <meta name="keywords" content={props.keywords} />
 
+      <link rel=”shortcut icon” href=”favicon.ico”>
+      <link rel=”apple-touch-icon-precomposed” href=”icon57.png”>
+
       <meta property="og:type" content="website" />
       <meta property="og:title" content={props.title} />
       <meta property="og:site_name" content={props.title} />
       <meta property="og:description" content={props.description} />
       <meta property="og:image" content={props.imgsrc} />
       <meta property="og:url" content={props.url} />
+
+      <meta property="og:image" content="https://example.com/image.jpg">
 
       <meta name="twitter:title" content={props.title} />
       <meta name="twitter:description" content={props.description} />
