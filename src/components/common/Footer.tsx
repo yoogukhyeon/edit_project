61115e5d138b7912bce1e35a382f6af96bf10b30
유국현 <yoogukhyeon@yugughyeon-ui-iMac.local>
2023-12-19 16:16:38 +0900

    push

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="w-full space-y-3 mb-20 text-center max-w-screen-xl mx-auto p-6 md:py-8">
-      <span className="block text-base max-sm:text-xs text-gray-500  dark:text-gray-400">© Copyright 2023 👤 유국현 All Rights Reserved.</span>
-      <span className="block text-base max-sm:text-xs text-gray-500  dark:text-gray-400">Contact US : rnrgus5897@gmail.com</span>
+    <footer className="w-full space-y-3 mb-28 text-center max-w-screen-xl mx-auto p-6 md:py-8">
+      <span className="block text-base max-sm:text-xs text-gray-500  dark:text-white">
+        © Copyright 2023 👤 <span className="font-bold dark:text-sky-400">유국현</span> All Rights Reserved.
+      </span>
+      <span className="block text-base max-sm:text-xs text-gray-500  dark:text-white">
+        Contact US : <span className="font-bold dark:text-sky-400">rnrgus5897@gmail.com</span>
+      </span>
     </footer>
   );
 };
