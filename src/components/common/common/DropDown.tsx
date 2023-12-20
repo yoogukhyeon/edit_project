b8fbecb3a91adc1574d9399fe466c566943c4607
유국현 <yoogukhyeon@yugughyeon-ui-iMac.local>
2023-12-20 09:40:27 +0900

    fix: UI 수정

--- src/components/common/common/DropDown.tsx
+++ src/components/common/common/DropDown.tsx
@@ -7,17 +7,17 @@ import { MdSportsSoccer } from 'react-icons/md';
 const DropDown = () => {
   return (
     <Dropdown label="ABOUT ME" style={{ width: '200px', color: 'skyblue' }}>
-      <Dropdown.Header>
-        <span className="block text-sm">유국현</span>
+      <Dropdown.Header className="px-4">
+        <span className="block text-sm">개발자 유국현</span>
         <span className="block truncate text-sm font-medium">rnrgus5897@gmail.com</span>
       </Dropdown.Header>
-      <Dropdown.Item as="a" href="https://flowbite.com/" target="_blank" icon={FaGithub}>
+      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={FaGithub}>
         Git
       </Dropdown.Item>
-      <Dropdown.Item as="a" href="https://flowbite.com/" target="_blank" icon={TbBrandBlogger}>
+      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={TbBrandBlogger}>
         blog
       </Dropdown.Item>
-      <Dropdown.Item as="a" href="https://flowbite.com/" target="_blank" icon={MdSportsSoccer}>
+      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={MdSportsSoccer}>
         business
       </Dropdown.Item>
     </Dropdown>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [],
   darkMode: 'class',
 };
