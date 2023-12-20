4f7f11f84ef698e0a189d2e31a4462803a1c92ff
유국현 <yoogukhyeon@yugughyeon-ui-iMac.local>
2023-12-20 09:46:00 +0900

    push

--- src/components/common/common/DropDown.tsx
+++ src/components/common/common/DropDown.tsx
@@ -6,21 +6,23 @@ import { MdSportsSoccer } from 'react-icons/md';
 
 const DropDown = () => {
   return (
-    <Dropdown label="ABOUT ME" style={{ width: '200px', color: 'skyblue' }}>
-      <Dropdown.Header className="px-4">
-        <span className="block text-sm">개발자 유국현</span>
-        <span className="block truncate text-sm font-medium">rnrgus5897@gmail.com</span>
-      </Dropdown.Header>
-      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={FaGithub}>
-        Git
-      </Dropdown.Item>
-      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={TbBrandBlogger}>
-        blog
-      </Dropdown.Item>
-      <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={MdSportsSoccer}>
-        business
-      </Dropdown.Item>
-    </Dropdown>
+    <div className="max-sm:hidden">
+      <Dropdown label="ABOUT ME" style={{ width: '200px', color: 'skyblue' }}>
+        <Dropdown.Header className="px-4">
+          <span className="block text-sm">개발자 유국현</span>
+          <span className="block truncate text-sm font-medium">rnrgus5897@gmail.com</span>
+        </Dropdown.Header>
+        <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={FaGithub}>
+          Git
+        </Dropdown.Item>
+        <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={TbBrandBlogger}>
+          blog
+        </Dropdown.Item>
+        <Dropdown.Item className="px-4" as="a" href="https://flowbite.com/" target="_blank" icon={MdSportsSoccer}>
+          business
+        </Dropdown.Item>
+      </Dropdown>
+    </div>
   );
 };
 
